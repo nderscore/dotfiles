6146c07bb5fcaaaf6bd3d157bef0cb198ca8da47
_nderscore <_@nderscore.com>
2022-03-21 00:52:41 -0400

    Add refresh rate

--- .screenlayout/gaming.sh
+++ .screenlayout/gaming.sh
@@ -9,4 +9,4 @@
 #           |   2   |
 #           |_______|
 
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x1440 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x1440 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-5 --off
