39d2ffedf330539cc0472efe79dde949a30c991e
_nderscore <_@nderscore.com>
2022-04-13 22:42:31 -0400

    Reconfigure monitors

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -2,9 +2,9 @@
 #
 # default layout
 #  _________
-# |         |________
+# |         |_______
 # |    1    |   2   |
 # |_________|_______|
-#           
+#
 
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --rate 144 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 2560x360 --rotate normal
--- .screenlayout/gaming.sh
+++ .screenlayout/gaming.sh
@@ -5,8 +5,9 @@
 #  _________
 # |         |
 # |    1    | (mouse can't move to second monitor except from bottom right corner)
-# |_________|________
+# |_________|_______
 #           |   2   |
 #           |_______|
+#
 
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x1440 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-5 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 2560x1440 --rotate normal
