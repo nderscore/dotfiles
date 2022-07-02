fe8535bbbc68ee98f86cccffbd35cdee17b0bdd3
_nderscore <_@nderscore.com>
2022-07-02 14:21:18 -0400

    Don't adjust screen layout or keyboard when playing controller games

--- .scripts/gamemode-start.sh
+++ .scripts/gamemode-start.sh
@@ -6,11 +6,17 @@ if [[ -f ~/.swaying ]]; then
     export SWAYSOCK="$(cat ~/.swaying)"
 fi
 
-# enable gaming screen layout
-~/.screenlayout/gaming.sh
+DS4_ID="$(bluetoothctl paired-devices | grep -Po '[[:alnum:]]{2}(:[[:alnum:]]{2})+(?= DualShock4)')"
+CONTROLLER_CONNECTED="$(bluetoothctl info $DS4_ID | grep -c 'Connected: yes')"
 
-# enable win/alt key swap
-~/.scripts/swap-win-alt.sh ON
+# enable screen & keyboard modifications only if game controller is not connected
+if [ $CONTROLLER_CONNECTED == "0" ]; then
+    # enable gaming screen layout
+    ~/.screenlayout/gaming.sh
+
+    # enable win/alt key swap
+    ~/.scripts/swap-win-alt.sh ON
+fi
 
 # send notification
 notify-send -u low "Game mode started."
