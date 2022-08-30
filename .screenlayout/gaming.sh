ce82dea601fbc2acb2399b9a1b8ad05339c6fb79
_nderscore <_@nderscore.com>
2022-08-30 18:35:53 -0400

    add TearFree xrandr flag on x11

--- .screenlayout/gaming.sh
+++ .screenlayout/gaming.sh
@@ -14,6 +14,7 @@ SESSION_TYPE="$(~/.scripts/get-session-type.sh)"
 
 if [[ $SESSION_TYPE == "x11" ]]; then
     xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 2560x1440 --rotate normal
+    xrandr --output DisplayPort-1 --set TearFree on --output HDMI-A-0 --set TearFree on
 elif [[ $SESSION_TYPE == "wayland" ]]; then
     swaymsg output DP-2 mode 2560x1440@144Hz pos 0 0
     swaymsg output HDMI-A-1 mode 1920x1080 pos 2560 1440
