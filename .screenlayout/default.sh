87d4a6f780bd106be6abc4dffad3083cb72cbe40
_nderscore <_@nderscore.com>
2023-06-23 21:24:55 -0400

    new monitor config

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 #
 # default layout
-#  _________
-# |         |_______
-# |    1    |   2   |
-# |_________|_______|
+#  _________ _________
+# |         |         |
+# |    1    |    2    |
+# |_________|_________|
 #
 
 SESSION_TYPE="$(~/.scripts/get-session-type.sh)"
 
 if [[ $SESSION_TYPE == "x11" ]]; then
-    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 2560x360 --rotate normal
-    xrandr --output DisplayPort-1 --set TearFree on --output HDMI-A-0 --set TearFree on
+    xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --rate 144 --pos 2560x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
+    xrandr --output DisplayPort-0 --set TearFree on --output DisplayPort-1 --set TearFree on
+
 elif [[ $SESSION_TYPE == "wayland" ]]; then
-    swaymsg output DP-2 mode 2560x1440@144Hz pos 0 0
-    swaymsg output HDMI-A-1 mode 1920x1080 pos 2560 360
+    swaymsg output DP-1 mode 2560x1440@144Hz pos 0 0
+    swaymsg output DP-2 mode 2560x1440@144Hz pos 2560 0
     swaymsg output \* max_render_time off
 fi
--- .screenlayout/gaming.sh
+++ .screenlayout/gaming.sh
@@ -5,18 +5,19 @@
 #  _________
 # |         |
 # |    1    | (mouse can't move to second monitor except from bottom right corner)
-# |_________|_______
-#           |   2   |
-#           |_______|
+# |_________|_________
+#           |         |
+#           |    2    |
+#           |_________|
 #
 
 SESSION_TYPE="$(~/.scripts/get-session-type.sh)"
 
 if [[ $SESSION_TYPE == "x11" ]]; then
-    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 2560x1440 --rotate normal
-    xrandr --output DisplayPort-1 --set TearFree on --output HDMI-A-0 --set TearFree on
+    xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --rate 144 --pos 2560x1440 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --off
+    xrandr --output DisplayPort-0 --set TearFree on --output DisplayPort-1 --set TearFree on
 elif [[ $SESSION_TYPE == "wayland" ]]; then
-    swaymsg output DP-2 mode 2560x1440@144Hz pos 0 0
-    swaymsg output HDMI-A-1 mode 1920x1080 pos 2560 1440
-    swaymsg output \* max_render_time off
+    swaymsg output DP-1 mode 2560x1440@144Hz pos 0 0
+    swaymsg output DP-2 mode 2560x1440@144Hz pos 2560 1440
+   swaymsg output \* max_render_time off
 fi
