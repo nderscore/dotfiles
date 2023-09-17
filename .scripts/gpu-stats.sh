1b3608ceecab664439fc9b7f61df2a75fa34edd9
_nderscore <_@nderscore.com>
2023-09-17 15:05:42 -0400

    Update GPU stats script

--- .scripts/gpu-stats.sh
+++ .scripts/gpu-stats.sh
@@ -5,12 +5,12 @@
 GIGABYTE=1073741824
 
 if [ -z "$STATS_GPU_TOTAL_MEM" ]; then
-    STATS_GPU_TOTAL_MEM="$(cat /sys/class/drm/card0/device/mem_info_vram_total)"
+    STATS_GPU_TOTAL_MEM="$(cat /sys/class/drm/card1/device/mem_info_vram_total)"
     export STATS_GPU_TOTAL_MEM="$(echo "scale=2; ${STATS_GPU_TOTAL_MEM}/${GIGABYTE}" | bc -l)"
 fi
 
-STATS_GPU_UTILIZATION="$(cat /sys/class/drm/card0/device/gpu_busy_percent)"
-STATS_GPU_MEM="$(cat /sys/class/drm/card0/device/mem_info_vram_used)"
+STATS_GPU_UTILIZATION="$(cat /sys/class/drm/card1/device/gpu_busy_percent)"
+STATS_GPU_MEM="$(cat /sys/class/drm/card1/device/mem_info_vram_used)"
 STATS_GPU_MEM="$(echo "scale=2; ${STATS_GPU_MEM}/${GIGABYTE}" | bc -l)"
 
 echo "🎨 ${STATS_GPU_UTILIZATION}% (${STATS_GPU_MEM}GiB)"
